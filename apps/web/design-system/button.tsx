73676ee1babf2ed63dfe186c7d3e880b86ebebae
stevenliao <steven.liaouey@gmail.com>
2023-05-08 22:00:54 -0400

    refactor: clean up

--- apps/web/design-system/button.tsx
+++ apps/web/design-system/button.tsx
@@ -3,7 +3,7 @@ import { Button as AriaButton, ButtonProps } from 'react-aria-components'
 import { VariantProps, cva } from 'class-variance-authority'
 
 const buttonStyles = cva(
-	'inline-flex items-center gap-2 font-semibold transition outline-none relative rounded-md  focus-visible:ring focus-visible:ring-indigo-600 focus-visible:ring-offset-2',
+	'inline-flex items-center gap-2 font-semibold transition outline-none relative rounded-md focus-visible:ring focus-visible:ring-indigo-600 focus-visible:ring-offset-2',
 	{
 		variants: {
 			color: {
